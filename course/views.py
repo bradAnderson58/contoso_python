d2af1b53d4ab16c8124f88cca2b559a68866dbca
bradAnderson58 <brad.anderson58@gmail.com>
2022-02-03 16:24:20 -0500

    Override perform_create in CourseListCreateView
    For the same reason as the perform_update in the previous commit, we have to manually add instructors to the course

--- course/views.py
+++ course/views.py
@@ -9,6 +9,13 @@ class CourseListCreateView(ListCreateAPIView):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
+    def perform_create(self, serializer):
+        course = serializer.save()
+        if 'instructors' in serializer.initial_data:
+            instructor_list = serializer.initial_data.pop('instructors')
+            instructors = [Person.objects.get(id=i.get('id')) for i in instructor_list]
+            course.instructors.set(instructors)
+
 
 class CourseView(RetrieveUpdateDestroyAPIView):
     queryset = Course.objects.all()
